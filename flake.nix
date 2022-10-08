46cc3e1f2887658255b8f873bdcc3cae07741659
Mia Jasmin Bautista Sanchez <mail@pyrolagus.de>
2022-10-08 16:56:36 +0200

    use imports array for homemanager

--- flake.nix
+++ flake.nix
@@ -57,7 +57,9 @@
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.pyrolagus = import ./user/pyrolagus/home.nix;
+            home-manager.users.pyrolagus.imports = [
+              ./user/pyrolagus/home.nix
+            ];
           }
         ];
       };
