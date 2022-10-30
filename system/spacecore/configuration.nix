b18f21a9d54568a146d0048721a137dcde421d87
Mia Jasmin Bautista Sanchez <mail@pyrolagus.de>
2022-10-30 14:12:05 +0100

    enable wireplumber

--- system/spacecore/configuration.nix
+++ system/spacecore/configuration.nix
@@ -109,6 +109,10 @@
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
+
+      wireplumber = {
+        enable = true;
+      };
     };
           
     #usbguard = {
