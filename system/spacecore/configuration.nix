69811e0c286886d34b090530704a0a5e32296663
Mia Jasmin Bautista Sanchez <mail@pyrolagus.de>
2022-11-09 15:01:59 +0100

    fix indentation

--- system/spacecore/configuration.nix
+++ system/spacecore/configuration.nix
@@ -20,7 +20,7 @@
         vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
         vaapiVdpau
         libvdpau-va-gl
-        ];
+      ];
     };
     
     opentabletdriver = {
