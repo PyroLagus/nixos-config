0136910592ee760f5caf2631e3fb1e65cbb7d788
Danny Bautista Sanchez <mail@pyrolagus.de>
2022-11-10 22:28:50 +0100

    blubb

---
+++ user/pyrolagus/audio.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, lib, ... }:
+{
+  services.fluidsynth = {
+    enable = true;
+    soundService = "pipewire-pulse";
+    extraOptions = [
+      "--connect-jack-outputs"
+      "--midi-driver=jack"
+      "-o midi.autoconnect=1"
+    ];
+  };
+
+  services.easyeffects = {
+    enable = true;
+  };
+
+  systemd.user.services.mpris-proxy = {
+    Unit.Description = "Mpris proxy";
+    Unit.After = [ "network.target" "sound.target" ];
+    Service.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+    Install.WantedBy = [ "default.target" ];
+  };
+}
