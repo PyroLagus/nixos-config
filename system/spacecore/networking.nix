b7097b5b3437c92419a9f0e040af6659c810e43f
Danny Bautista Sanchez <mail@pyrolagus.de>
2022-11-10 18:37:00 +0100

    blubb

--- system/spacecore/networking.nix
+++ system/spacecore/networking.nix
@@ -1,15 +1,5 @@
 { config, pkgs, lib, nixpkgs, ... }:
 
-let
-  wlanDevice = {
-    name = "wlan0";
-    address = "e4:aa:ea:f8:57:5f";
-  };
-  lanDevice = {
-    name = "lan0";
-    address = "a0:ce:c8:c9:54:03";
-  };
-in
 {
   pcfg.networking = {
     enable = true;
@@ -34,37 +24,7 @@ in
       enable = true;
       userControlled.enable = true;
     };
-
-    /*
-    # Use enable DHCP explicitly for each device.
-    useDHCP = false;
-    interfaces = {
-      "${lanDevice.name}".useDHCP = true;
-      "${wlanDevice.name}".useDHCP = true;
-    };
-    */
   };
-
-  /*
-
-  systemd.services = {
-    # don't require ethernet to be connected when booting
-    "network-link-${lanDevice.name}".wantedBy = lib.mkForce [ ];
-    "network-addresses-${lanDevice.name}".wantedBy = lib.mkForce [ ];
-  };
-  */
-
-/*
-  systemd.network.links."10-lan" = {
-    matchConfig.PermanentMACAddress = "${lanDevice.address}";
-    linkConfig.Name = "${lanDevice.name}";
-  };
-
-  systemd.network.links."10-wlan" = {
-    matchConfig.PermanentMACAddress = "${wlanDevice.address}";
-    linkConfig.Name = "${wlanDevice.name}";
-  };
-*/
 
   services = {
     resolved = {
