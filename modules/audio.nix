529ddc7a433899ffdef4a1c472353a98031322c5
Mia Jasmin Bautista Sanchez <mail@pyrolagus.de>
2022-11-15 19:23:27 +0100

    bluez5 for wireplumber

--- modules/audio.nix
+++ modules/audio.nix
@@ -15,7 +15,7 @@
 
       wireplumber = {
         enable = true;
-        package = (pkgs.wireplumber.override { bluez = pkgs.bluez });
+        package = pkgs.wireplumber.override { bluez = pkgs.bluez; };
       };
     };
     security.rtkit.enable = true;
