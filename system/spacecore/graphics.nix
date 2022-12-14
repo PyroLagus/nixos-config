c6428e57b06077a8f72bc084f36cf4bda0a7c295
Danny Bautista Sanchez <mail@pyrolagus.de>
2022-12-14 11:32:06 +0100

    try amdvlk

--- system/spacecore/graphics.nix
+++ system/spacecore/graphics.nix
@@ -82,6 +82,7 @@
         vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
         vaapiVdpau
         libvdpau-va-gl
+        amdvlk
       ];
     };
     opentabletdriver = {
