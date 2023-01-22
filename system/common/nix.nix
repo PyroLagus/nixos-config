31bba5732f485e1c9ed84c107f3c299edea07e61
Mia Jasmin Bautista Sanchez <mail@pyrolagus.de>
2023-01-22 02:34:45 +0100

    enable automatic garbage collection

--- system/common/nix.nix
+++ system/common/nix.nix
@@ -5,6 +5,11 @@
     settings = {
       auto-optimise-store = true;
     };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
