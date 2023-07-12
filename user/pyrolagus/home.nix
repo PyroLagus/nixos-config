7fcff5b4a59b39a8fa137f05122674db1ed72da7
Danny Bautista Sanchez <mail@pyrolagus.de>
2023-07-12 20:10:43 +0200

    Set firefox as default browser explicitly.

--- user/pyrolagus/home.nix
+++ user/pyrolagus/home.nix
@@ -99,7 +99,7 @@ in
     wireshark
 
     flips
-    
+
     retroarchFull
 
 
@@ -276,10 +276,32 @@ in
   xdg = {
     mimeApps = {
       enable = true;
+      associations.added = {
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "x-scheme-handler/chrome" = "firefox.desktop";
+        "text/html" = "firefox.desktop";
+        "application/x-extension-htm" = "firefox.desktop";
+        "application/x-extension-html" = "firefox.desktop";
+        "application/x-extension-shtml" = "firefox.desktop";
+        "application/xhtml+xml" = "firefox.desktop";
+        "application/x-extension-xhtml" = "firefox.desktop";
+        "application/x-extension-xht" = "firefox.desktop";
+      };
       defaultApplications = {
         "application/pdf" = "org.pwmt.zathura.desktop";
         "application/mp4" = "mpv.desktop";
         "video/x-matroska" = "mpv.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "x-scheme-handler/chrome" = "firefox.desktop";
+        "text/html" = "firefox.desktop";
+        "application/x-extension-htm" = "firefox.desktop";
+        "application/x-extension-html" = "firefox.desktop";
+        "application/x-extension-shtml" = "firefox.desktop";
+        "application/xhtml+xml" = "firefox.desktop";
+        "application/x-extension-xhtml" = "firefox.desktop";
+        "application/x-extension-xht" = "firefox.desktop";
       };
     };
   };
