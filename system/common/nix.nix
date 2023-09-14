08c4e5840772e73c3f69fef4d768bd6703019529
Danny Bautista Sanchez <mail@pyrolagus.de>
2023-09-15 00:56:13 +0200

    Add content addressed substituter.

--- system/common/nix.nix
+++ system/common/nix.nix
@@ -4,8 +4,8 @@
     package = pkgs.nixVersions.stable;
     settings = {
       auto-optimise-store = true;
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      substituters = [ "https://hyprland.cachix.org" "https://cache.ngi0.nixos.org/" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA=" ];
     };
     gc = {
       automatic = true;
