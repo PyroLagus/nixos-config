aa595b122763290fb4404e4270e825ded5463448
Mia Jasmin Bautista Sanchez <mail@pyrolagus.de>
2024-06-07 15:30:08 +0200

    configure external wlan adapter

--- system/spacecore/networking.nix
+++ system/spacecore/networking.nix
@@ -5,10 +5,18 @@
     enable = true;
     interfaces = {
       "wlan0" = {
+        enable = false;
         hwAddress = "e4:aa:ea:f8:57:5f";
         required = true;
         isWireless = true;
       };
+
+      "wlan1" =
+        {
+          hwAddress = "24:2f:d0:d9:30:cd";
+          required = false;
+          isWireless = true;
+        };
 
       "lan0" = {
         hwAddress = "26:61:32:e6:33:3a";
